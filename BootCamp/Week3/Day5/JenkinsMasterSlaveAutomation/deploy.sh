c2b8371cf49bdee144113dfbcb8b4314cc765142
pxkundu <pxkundu2@shockers.wichita.edu>
2025-03-17 00:06:58 -0400

    Updated Script with new config

--- BootCamp/Week3/Day5/JenkinsMasterSlaveAutomation/deploy.sh
+++ BootCamp/Week3/Day5/JenkinsMasterSlaveAutomation/deploy.sh
@@ -3,5 +3,5 @@ aws cloudformation validate-template --template-body file://templates/jenkins-ma
 aws cloudformation create-stack \
   --stack-name JenkinsMasterSlave \
   --template-body file://templates/jenkins-master-slave.yaml \
-  --parameters ParameterKey=KeyName,ParameterValue=<your-key-name> \
+  --parameters ParameterKey=KeyName,ParameterValue=TrainingSetup \
   --region us-east-1
