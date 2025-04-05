2dd74bb30e6e582432a76e06a11646176eac7208
pxkundu <pxkundu2@shockers.wichita.edu>
2025-04-04 22:25:13 -0400

    Updated content for week 6 Day 3 as Phase 3 of Capstone Project

--- BootCamp/Week6/CapstoneProject/ecomm-capstone/backend/src/index.js
+++ BootCamp/Week6/CapstoneProject/ecomm-capstone/backend/src/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const AWSXRay = require('aws-xray-sdk');
+const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 const app = express();
+const cloudwatch = new AWS.CloudWatch();
+
+AWSXRay.captureHTTPsGlobal(require('http'));
+app.use(AWSXRay.express.openSegment('EcommBackend'));
 
 app.get('/inventory', (req, res) => res.json({ products: 10000 }));
-app.get('/orders', (req, res) => res.json({ orders: 0 }));
+app.get('/orders', (req, res) => {
+  cloudwatch.putMetricData({
+    MetricData: [{ MetricName: 'OrdersPerMinute', Value: 1, Unit: 'Count' }],
+    Namespace: 'EcommMetrics'
+  }).promise();
+  res.json({ orders: 0 });
+});
 app.get('/health', (req, res) => res.status(200).send('OK'));
 app.get('/ready', (req, res) => res.status(200).send('Ready'));
 
+app.use(AWSXRay.express.closeSegment());
 app.listen(80, () => console.log('Backend running on port 80'));
